735618d5c861129b89c3a810659a31c72f8c019d
viizz29 <viizz29@gmail.com>
2024-01-23 12:23:45 -0500

    added receipt entry pages, frontend only, with some errors

--- frontend/src/utils/types/chequebook_master_types.ts
+++ frontend/src/utils/types/chequebook_master_types.ts
@@ -15,3 +15,16 @@ export type ChequebookTableData = {
 };
 
 
+export interface AddChequebookDetailsData {
+  date: string,
+  bank_branch: string,
+  bank_name: string,
+  bank_account_number: string,
+  cheque_no_from: string,
+  cheque_no_to: string,
+  employee_name: string,
+  issuer_name: string,
+  ifsc_code: string,
+  page_count: number,
+  remarks: string,
+}
