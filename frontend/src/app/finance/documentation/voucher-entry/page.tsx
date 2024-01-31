8c7656f520111693b5cc826fd78f5d9a222d9a64
Krish vishwakarma <kkrish7654@gmail.com>
2024-01-31 11:53:16 +0530

    voucher entry UI done

---
+++ frontend/src/app/finance/documentation/voucher-entry/page.tsx
@@ -0,0 +1,12 @@
+
+import { HeroVoucherEntry } from "@/components/JuidcoFinance/Documentation/VoucherEntry/Index";
+import PageLayout from "@/components/Layouts/PageLayout";
+import React from "react";
+
+export default function Home() {
+  return (
+    <PageLayout>
+        <HeroVoucherEntry/>
+    </PageLayout>
+  );
+}
