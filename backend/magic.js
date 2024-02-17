5f562ba8f5fbc2486e793c5ebf429e6bcced84b0
viizz29 <viizz29@gmail.com>
2024-02-17 19:01:56 +0530

    small changes in magic script

--- backend/magic.js
+++ backend/magic.js
@@ -1,4 +1,5 @@
-
+const fs = require('fs');
+    
 const showHelp = () => {
     console.log("\n\nSyntax: node magic.js <command>")
     console.log("\ncommands:");
@@ -8,17 +9,36 @@ const showHelp = () => {
 }
 
 
-const mergeSchemas = () => {
+const getSchemaFileList = async (folder, list) => {
+
+    const items = fs.readdirSync(folder);
+    for (let i=0;i<items.length;i++){
+        const file = items[i];
+        const x = folder+"/"+file;
+        if(fs.lstatSync(x).isDirectory()){
+            await getSchemaFileList(x, list);
+        }else{
+            list.push(x);
+        }
+    }
+}
+
+
+const mergeSchemas = async () => {
+    const schemaFolder = "./prisma/schemas";
+    const list = [];
+    await getSchemaFileList(schemaFolder, list);
+    console.log(list);
+    
 }
 
 
 const args = process.argv;
-if(args.length < 3){console.log("Please provide an argument ...");showHelp();}
-else
-{
+if (args.length < 3) { console.log("Please provide an argument ..."); showHelp(); }
+else {
     const command = args[2];
-    
-    if(command == "merge"){mergeSchemas();}
 
-    else{console.log("Unknown command: " + command);showHelp();}
+    if (command == "merge") { mergeSchemas(); }
+
+    else { console.log("Unknown command: " + command); showHelp(); }
 }
