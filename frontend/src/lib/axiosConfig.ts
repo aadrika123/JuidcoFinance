5fe0aee3913bda6d4b14d73c1d7a3537904055d3
sanjiv kumar <sanjivkumarhzb1427@gmail.com>
2024-03-19 18:15:59 +0530

    Changes in login page

--- frontend/src/lib/axiosConfig.ts
+++ frontend/src/lib/axiosConfig.ts
@@ -1,12 +1,18 @@
 // axiosConfig.js
 
 import axios from "axios";
+import Cookies from "js-cookie";
 
+const userData = Cookies.get("loginData");
+let user: any = "";
+if (userData) {
+  user = JSON.parse(userData);
+}
 const instance = axios.create({
   // baseURL: "http://teamwork.ddnsfree.com:3001/api/v1/finance",
   baseURL: "/api/v1/finance",
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoRGF0YSI6eyJpZCI6MSwidXNlcl9pZCI6InExZXRVNiIsIm5hbWUiOiJKb2huIFNtaXRoIiwiZW1haWwiOiJIYXlsZXlfTGFya2luQGhvdG1haWwuY29tIiwiZGVzaWduYXRpb24iOnsiaWQiOjEsIm5hbWUiOiJQcm9qZWN0IE1hbmFnZXIifX0sImlhdCI6MTcwOTEwMzk1MCwiZXhwIjoxNzA5MzYzMTUwfQ.BKaGCBp8aADFtAj9qTWJ7jOUr6udFc5m_zNxTktXJcA`,
+    Authorization: `Bearer ${user?.token}`,
   },
   // You can add other default configurations here
 });
