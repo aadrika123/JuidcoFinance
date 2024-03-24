58fab0dd097a5c496232b0221e44af25f0eaf289
viizz29 <viizz29@gmail.com>
2024-03-24 09:52:40 +0530

    modified build and start commands

--- backend/build/component/juidcoFinance/responseMessage/vendorTypeMessage.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ResMessage = Object.freeze({
-    NOT_FOUND: "Vendor Type Not Found",
-    FOUND: "Vendor Type Found Successfully!!"
-});
-exports.default = ResMessage;
