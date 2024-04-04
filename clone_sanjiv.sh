aee5fad23a97f00b02ac330f73d06935d4dbeb5c
sanjiv kumar <sanjivkumarhzb1427@gmail.com>
2024-04-04 12:37:19 +0530

    making ready foreign_fdw for staging

--- clone_sanjiv.sh
+++ clone_sanjiv.sh
@@ -8,8 +8,6 @@ GITHUB_USER=sanjivgit
 GITHUB_TOKEN=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
 
 
-
-
 cloneRepo(){
     #  git clone -b viizz_dev https://viizz29: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@github.com/aadrika123/JuidcoFinance
     git clone -b $GITHUB_BRANCH https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$GITHUB_REPO
