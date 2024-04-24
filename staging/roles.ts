2ddb00af6747751e46eb1418d0b18ed7f260d2a9
sanjiv kumar <sanjivkumarhzb1427@gmail.com>
2024-04-24 14:03:00 +0530

    added role file in stage folder

---
+++ staging/roles.ts
@@ -0,0 +1,12 @@
+import { sidebarJuniorEngLinks } from "./sidebar.junior.engeneer.json";
+
+export const ROLES = Object.freeze({
+  ACC_DEP_MANAGER: "Accounts Department - Manager",           ///// Accounts Department – Manager   and Accounts Department - Manager     is not same So Please Copy it from database
+  ACC_DEP_ACCOUNTANT: "Accounts Department - Accountant",
+  PROJECT_DIR_FINANCE: "Project Director Finance",
+});
+
+
+export const TEMP_ROLE = Object.freeze({
+    JUNIOR: sidebarJuniorEngLinks
+})
