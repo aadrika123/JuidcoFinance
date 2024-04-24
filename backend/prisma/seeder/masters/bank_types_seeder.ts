112a46e128194fa5cf0e9bc2537115586cc15a44
viizz29 <viizz29@gmail.com>
2024-04-24 13:49:37 +0530

    updated bank type list

--- backend/prisma/seeder/masters/bank_types_seeder.ts
+++ backend/prisma/seeder/masters/bank_types_seeder.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, account_codes } from "@prisma/client";
 import { faker } from "@faker-js/faker";
 const prisma = new PrismaClient();
 const bank_types_seeder = async () => {
@@ -8,19 +8,40 @@ const bank_types_seeder = async () => {
   //   "4502300 - Schedule Co-operative Banks - Municipal Fund",
   // ];
 
-  const codeItems = ["4502100", "4502200", "4502300"];
+  const codeItems: number[] = [
+    4502100,
+    4502200,
+    4502300,
+    4502400,
+    4502500,
+    4504100,
+    4504200,
+    4504300,
+    4504400,
+    4504500,
+    4506100,
+    4506200,
+    4506300,
+    4506400,
+    4506500
+  ];
+
+  
+
 
   codeItems.forEach(async (item, i) => {
     const data: any = await prisma.account_codes.findFirst({
       where: {
-        code: codeItems[i],
+        code: ""+codeItems[i],
       },
     });
+
+    console.log(data);
 
     const record = {
       id: i + 1,
       primary_acc_code_id: data.id,
-      name: item,
+      name: `${data.code} ${data.description}`,
       created_at: faker.date.past(),
       updated_at: faker.date.recent(),
     };
