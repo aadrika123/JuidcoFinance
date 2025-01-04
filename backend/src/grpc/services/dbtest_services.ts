51c8ec66d688df5d01079f1a6995c8b3f02067b1
sahil raj <sahil27raj@gmail.com>
2025-01-04 11:04:14 +0530

    add the changes for handling the grpc data from the code

--- backend/src/grpc/services/dbtest_services.ts
+++ backend/src/grpc/services/dbtest_services.ts
@@ -1,64 +1,64 @@
-import {
-    ServerUnaryCall,
-    sendUnaryData,
-    ServerWritableStream,
-  } from "@grpc/grpc-js";
+// import {
+//     ServerUnaryCall,
+//     sendUnaryData,
+//     ServerWritableStream,
+//   } from "@grpc/grpc-js";
 
-import { PrismaClient, Prisma } from "@prisma/client";
+// import { PrismaClient, Prisma } from "@prisma/client";
 
-import { IDatabaseTesterServer } from "../proto/dbtest_grpc_pb";
-import { Bank, BankRequest, BanksRequest } from "../proto/dbtest_pb";
+// import { IDatabaseTesterServer } from "../proto/dbtest_grpc_pb";
+// import { Bank, BankRequest, BanksRequest } from "../proto/dbtest_pb";
   
 
 
-export const DatabaseTestServer: IDatabaseTesterServer = {
-  getBanksStreamed: async function (): Promise<void> {
+// export const DatabaseTestServer: IDatabaseTesterServer = {
+//   getBanksStreamed: async function (): Promise<void> {
     
-  },
-  getBank: async function (call: ServerUnaryCall<BankRequest, Bank>, callback: sendUnaryData<Bank>): Promise<void> {
-    const prisma = new PrismaClient();
+//   },
+//   getBank: async function (call: ServerUnaryCall<BankRequest, Bank>, callback: sendUnaryData<Bank>): Promise<void> {
+//     const prisma = new PrismaClient();
 
-    const id = call.request.getId();
+//     const id = call.request.getId();
 
-    const query: Prisma.banksFindManyArgs = {
-      where: {
-        id: id,
-      },
-      select: {
-        id: true,
-        name: true,
-      },
-    };
-    const bank: any = await prisma.banks.findFirst(query);
+//     const query: Prisma.banksFindManyArgs = {
+//       where: {
+//         id: id,
+//       },
+//       select: {
+//         id: true,
+//         name: true,
+//       },
+//     };
+//     const bank: any = await prisma.banks.findFirst(query);
 
-      const b = new Bank();
+//       const b = new Bank();
 
-      // console.log(bank);
+//       // console.log(bank);
 
-      if(bank != null){
-        b.setId(bank['id' as keyof typeof bank]);
-        b.setName(bank['name' as keyof typeof bank]);  
-      }
+//       if(bank != null){
+//         b.setId(bank['id' as keyof typeof bank]);
+//         b.setName(bank['name' as keyof typeof bank]);  
+//       }
 
-      callback(null, b);
-  },
-  getBanks: async function (call: ServerWritableStream<BanksRequest, Bank>): Promise<void> {
-    const prisma = new PrismaClient();
+//       callback(null, b);
+//   },
+//   getBanks: async function (call: ServerWritableStream<BanksRequest, Bank>): Promise<void> {
+//     const prisma = new PrismaClient();
 
-    const query: Prisma.banksFindManyArgs = {
-      select: {
-        id: true,
-        name: true,
-      },
-    };
-    const banks = await prisma.banks.findMany(query);
+//     const query: Prisma.banksFindManyArgs = {
+//       select: {
+//         id: true,
+//         name: true,
+//       },
+//     };
+//     const banks = await prisma.banks.findMany(query);
 
-    for (let i = 0; i < banks.length; i++) {
-      const b = new Bank();
-      b.setId(banks[i].id);
-      b.setName(banks[i].name);
-      call.write(b);
-    }
-    call.end();
-  }
-}
+//     for (let i = 0; i < banks.length; i++) {
+//       const b = new Bank();
+//       b.setId(banks[i].id);
+//       b.setName(banks[i].name);
+//       call.write(b);
+//     }
+//     call.end();
+//   }
+// }
--- backend/src/grpc/services/holding_and_tax_services.ts
+++ backend/src/grpc/services/holding_and_tax_services.ts
@@ -1,42 +1,42 @@
-import {
-    ServerUnaryCall,
-    sendUnaryData,
-  } from "@grpc/grpc-js";
+// import {
+//     ServerUnaryCall,
+//     sendUnaryData,
+//   } from "@grpc/grpc-js";
 
-import { IHoldingAndTaxServer } from "../proto/holding_and_tax_grpc_pb";
-import { DemandsOnExistingAssessments, GeneralResponse } from "../proto/holding_and_tax_pb";
+// import { IHoldingAndTaxServer } from "../proto/holding_and_tax_grpc_pb";
+// import { DemandsOnExistingAssessments, GeneralResponse } from "../proto/holding_and_tax_pb";
   
 
 
-export const HoldingAndTaxServer: IHoldingAndTaxServer = {
-  recordDemandsOnExistingAssessments: function (call: ServerUnaryCall<DemandsOnExistingAssessments, GeneralResponse>, callback: sendUnaryData<GeneralResponse>): void {
+// export const HoldingAndTaxServer: IHoldingAndTaxServer = {
+//   recordDemandsOnExistingAssessments: function (call: ServerUnaryCall<DemandsOnExistingAssessments, GeneralResponse>, callback: sendUnaryData<GeneralResponse>): void {
     
-    // define
-    console.log("ulb: " + call.request.getUlb());
-    console.log("bill no: " + call.request.getBillNo());
-    console.log("date bill raised: " + call.request.getDateBillRaised());
-    console.log("old ward no: " + call.request.getOldWardNo());
-    console.log("new ward no: " + call.request.getNewWardNo());
-    console.log("department/section: " + call.request.getDepartmentOrSection());
+//     // define
+//     console.log("ulb: " + call.request.getUlb());
+//     console.log("bill no: " + call.request.getBillNo());
+//     console.log("date bill raised: " + call.request.getDateBillRaised());
+//     console.log("old ward no: " + call.request.getOldWardNo());
+//     console.log("new ward no: " + call.request.getNewWardNo());
+//     console.log("department/section: " + call.request.getDepartmentOrSection());
 
-    const property_details = call.request.getPropertyDetails();
-    console.log("\n\nProperty details: ");
+//     const property_details = call.request.getPropertyDetails();
+//     console.log("\n\nProperty details: ");
 
-    console.log("Assessment code: " + property_details?.getAssessmentCode());
+//     console.log("Assessment code: " + property_details?.getAssessmentCode());
     
-    const accounting_entries = property_details?.getAccountingEntriesList();
-    console.log("Accounting entries: ");
-    accounting_entries?.forEach(item => {
-      console.log("Description: " + item.getDescription());
-      console.log("Credit Amount: " + item.getCreditAmount());
-      console.log("Debit Amount: " + item.getDebitAmount());
-      console.log("========================");
-    });
+//     const accounting_entries = property_details?.getAccountingEntriesList();
+//     console.log("Accounting entries: ");
+//     accounting_entries?.forEach(item => {
+//       console.log("Description: " + item.getDescription());
+//       console.log("Credit Amount: " + item.getCreditAmount());
+//       console.log("Debit Amount: " + item.getDebitAmount());
+//       console.log("========================");
+//     });
 
-    const res = new GeneralResponse()
-    .setCode(200)
-    .setMessage("OK");
+//     const res = new GeneralResponse()
+//     .setCode(200)
+//     .setMessage("OK");
     
-    callback(null, res);
-  },
-}
+//     callback(null, res);
+//   },
+// }
