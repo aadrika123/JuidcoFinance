36a0f5380d6a00da52dd71161ec08ffeea26ad55
sahil raj <sahil27raj@gmail.com>
2025-01-20 17:13:14 +0530

    add the changes for the baseurl

--- frontend/src/lib/axiosConfig.ts
+++ frontend/src/lib/axiosConfig.ts
@@ -4,11 +4,11 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://teamwork.ddnsfree.com:3001/api/v1/finance",
-  // baseURL: "https://aadrikainfomedia.com/",
-  baseURL: "http://localhost:8000/api/v1/finance",
-  // headers: {
-  //   Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoRGF0YSI6eyJpZCI6MSwidXNlcl9pZCI6InExZXRVNiIsIm5hbWUiOiJKb2huIFNtaXRoIiwiZW1haWwiOiJIYXlsZXlfTGFya2luQGhvdG1haWwuY29tIiwiZGVzaWduYXRpb24iOnsiaWQiOjEsIm5hbWUiOiJQcm9qZWN0IE1hbmFnZXIifX0sImlhdCI6MTcwOTEwMzk1MCwiZXhwIjoxNzA5MzYzMTUwfQ.BKaGCBp8aADFtAj9qTWJ7jOUr6udFc5m_zNxTktXJcA`,
-  // },
+  baseURL: " https://aadrikainfomedia.com",
+
+  headers: {
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoRGF0YSI6eyJpZCI6MSwidXNlcl9pZCI6InExZXRVNiIsIm5hbWUiOiJKb2huIFNtaXRoIiwiZW1haWwiOiJIYXlsZXlfTGFya2luQGhvdG1haWwuY29tIiwiZGVzaWduYXRpb24iOnsiaWQiOjEsIm5hbWUiOiJQcm9qZWN0IE1hbmFnZXIifX0sImlhdCI6MTcwOTEwMzk1MCwiZXhwIjoxNzA5MzYzMTUwfQ.BKaGCBp8aADFtAj9qTWJ7jOUr6udFc5m_zNxTktXJcA`,
+  },
   // You can add other default configurations here
 });
 
